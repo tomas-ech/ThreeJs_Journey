f8c0aaf3c5d6cbc4caa2dfad3d97e8877b06a665
tomas96ech <techeverri96@gmail.com>
2024-06-12 12:15:03 -0500

    lesson lights

--- ClassicTecniques/01_Lights/src/script.js
+++ ClassicTecniques/01_Lights/src/script.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
+import { RectAreaLightHelper } from 'three/examples/jsm/Addons.js'
 
 /**
  * Base
@@ -17,18 +18,74 @@ const scene = new THREE.Scene()
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 1.5)
-scene.add(ambientLight)
+// const ambientLight = new THREE.AmbientLight(0xffffff, 0)
+// scene.add(ambientLight)
+
+// gui
+//     .add(ambientLight, 'intensity')
+//     .min(0)
+//     .max(3)
+//     .step(0.01)
 
 const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.9) //Like the sun light
-directionalLight.position.set(1, 0.25, 0)
+directionalLight.position.set(3, 0.25, 0)
 scene.add(directionalLight)
 
-// const pointLight = new THREE.PointLight(0xffffff, 50)
-// pointLight.position.x = 2
-// pointLight.position.y = 3
-// pointLight.position.z = 4
-// scene.add(pointLight)
+const colorsArray = {}
+
+colorsArray.skyColor = 0xff0000
+colorsArray.groundColor = 0x0000ff
+
+const hemisphereLight = new THREE.HemisphereLight(colorsArray.skyColor, colorsArray.groundColor)
+
+scene.add(hemisphereLight)
+
+gui.addColor(colorsArray, 'skyColor')
+    .onChange(() => {
+        hemisphereLight.color = new THREE.Color(colorsArray.skyColor)
+    })
+
+gui.addColor(colorsArray, 'groundColor')
+    .onChange(() => {
+        hemisphereLight.groundColor = new THREE.Color(colorsArray.groundColor)
+    })
+
+const pointLight = new THREE.PointLight(0xffffff, 50)
+pointLight.position.x = 1
+pointLight.position.y = -1
+pointLight.position.z = 1
+scene.add(pointLight)
+
+// React area light
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 6, 1, 1)
+rectAreaLight.position.set(- 1.5, 0, 1.5)
+rectAreaLight.lookAt(new THREE.Vector3())
+scene.add(rectAreaLight)
+
+// Spot light
+const spotLight = new THREE.SpotLight(0x78ff00, 4.5, 10, Math.PI * 0.1, 0.25, 1)
+spotLight.position.set(0, 2, 3)
+scene.add(spotLight)
+
+
+/* 
+Helpers
+*/
+
+const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2)
+scene.add(hemisphereLightHelper)
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2)
+scene.add(directionalLightHelper)
+
+const rectAreaHelper = new RectAreaLightHelper(rectAreaLight)
+scene.add(rectAreaHelper)
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2)
+scene.add(pointLightHelper)
+
+const spotLightHelper = new THREE.SpotLightHelper(spotLight)
+scene.add(spotLightHelper)
 
 /**
  * Objects
@@ -72,8 +129,7 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -115,8 +171,7 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
+const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
