4a78f558a3a6d01b7b0df4bcd946f24bd67a8942
Tomas Echeverri <techeverri96@gmail.com>
2024-06-17 14:05:27 -0500

    galaxy generator base shape

---
+++ ClassicTecniques/05_Galaxy-Generator/src/script.js
@@ -0,0 +1,131 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import GUI from 'lil-gui'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Test cube
+ */
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial()
+// )
+// scene.add(cube)
+
+const parameters = {}
+parameters.count = 1000
+parameters.size = 0.02
+
+
+gui.add(parameters, 'count').min(100).max(20000).step(100).onFinishChange(generateGalaxy)
+gui.add(parameters, 'size').min(0.01).max(1).step(0.01).onFinishChange(generateGalaxy)
+
+const geometry = new THREE.BufferGeometry()
+
+const material = new THREE.PointsMaterial({
+    size: parameters.size,
+    sizeAttenuation: true,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending
+})
+
+function generateGalaxy() {
+    console.log("Holi, estoy Generando");
+
+    const positions = new Float32Array(parameters.count * 3)
+    
+    for (let index = 0; index < parameters.count; index++) {
+
+        const index3 = index * 3
+
+        positions[index3] = (Math.random() - 0.5) * 3
+        positions[index3 + 1] = (Math.random() - 0.5) * 3
+        positions[index3 + 2] = (Math.random() - 0.5) * 3
+
+        geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+    
+        const points =new THREE.Points(geometry, material)
+        scene.add(points)
+    }
+
+}
+
+generateGalaxy()
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 3
+camera.position.y = 3
+camera.position.z = 3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
