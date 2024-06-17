d193b4c64a3f2a464adc90c8a6e02f5fbe071472
Tomas Echeverri <techeverri96@gmail.com>
2024-06-17 13:35:09 -0500

    Particle final

--- ClassicTecniques/04_Particles/src/script.js
+++ ClassicTecniques/04_Particles/src/script.js
@@ -25,9 +25,9 @@ const particleTexture = textureLoader.load('textures/particles/4.png')
  * Particles
  */
 // Geometry
-const sphereGeometry = new THREE.SphereGeometry(1, 32, 32)
-const sphereMesh = new THREE.Mesh(sphereGeometry, new THREE.MeshBasicMaterial())
-scene.add(sphereMesh)
+// const sphereGeometry = new THREE.SphereGeometry(1, 32, 32)
+// const sphereMesh = new THREE.Mesh(sphereGeometry, new THREE.MeshBasicMaterial())
+// scene.add(sphereMesh)
 
 // Material
 const particlesMaterial = new THREE.PointsMaterial({
@@ -40,7 +40,8 @@ const particlesMaterial = new THREE.PointsMaterial({
     //alphaTest: 0.001, //we use a small number instead of 0, that way the render wont show any edges
     //depthTest: false
     depthWrite: false,
-    blending: THREE.AdditiveBlending
+    blending: THREE.AdditiveBlending,
+    vertexColors: true
 })
 gui.addColor(particlesMaterial, 'color')
 
@@ -48,15 +49,18 @@ gui.addColor(particlesMaterial, 'color')
  * Custom Geometry
  */
 const customGeometry = new THREE.BufferGeometry()
-const count = 500
+const count = 9000
 
+const colors = new Float32Array(count * 3)
 const positions = new Float32Array(count * 3) //Multiply by 3 due that each positions is composed of 3 values (x,y,z)
 
 for (let index = 0; index < count * 3; index++) {
     positions[index] = (Math.random() - 0.5) * 10
+    colors[index] = Math.random()
 }
 
 customGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3)) //This is used to inform the geometry that each position used 3 values
+customGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
 
 const customParticles = new THREE.Points(customGeometry, particlesMaterial)
 scene.add(customParticles)
@@ -113,6 +117,7 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    customParticles.rotation.y = elapsedTime * 0.1
 
     // Update controls
     controls.update()
