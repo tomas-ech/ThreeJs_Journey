4c7a50ee210bf19b3429ec03d37d9d631ef6655c
Tomas Echeverri <techeverri96@gmail.com>
2024-06-20 17:37:32 -0500

    scroll animations class completed

--- ClassicTecniques/06_Scroll-animations/src/script.js
+++ ClassicTecniques/06_Scroll-animations/src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import GUI from "lil-gui";
+import { gsap } from "gsap";
 
 /**
  * Debug
@@ -10,21 +11,20 @@ const parameters = {
   materialColor: "#00ccff",
 };
 
-gui.addColor(parameters, "materialColor").onFinishChange(() => {
-  material.color.set(parameters.materialColor);
-});
-
+// gui.addColor(parameters, "materialColor").onFinishChange(() => {
+//   material.color.set(parameters.materialColor);
+// });
 
 /**
  * Textures
-*/
+ */
 const textureLoader = new THREE.TextureLoader();
 const gradientTexture = textureLoader.load("textures/gradients/3.jpg");
 gradientTexture.magFilter = THREE.NearestFilter;
 
 /**
  * Base
-*/
+ */
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
@@ -33,11 +33,11 @@ const scene = new THREE.Scene();
 
 //Materials
 const material = new THREE.MeshNormalMaterial({
-    color: parameters.materialColor,
-    gradientMap: gradientTexture,
-    wireframe: true
+  //color: parameters.materialColor,
+  gradientMap: gradientTexture,
+  wireframe: true,
 });
-gui.add(material, "wireframe")
+gui.add(material, "wireframe");
 
 /**
  * Meshes
@@ -52,13 +52,13 @@ const knotTorus = new THREE.Mesh(
 );
 
 torus.position.y = -objectsDistance * 0;
-torus.position.x = 2
+torus.position.x = 2;
 
 piramid.position.y = -objectsDistance * 1;
-piramid.position.x = -2
+piramid.position.x = -2;
 
 knotTorus.position.y = -objectsDistance * 2;
-knotTorus.position.x = 2
+knotTorus.position.x = 2;
 
 const sectionMeshes = [torus, piramid, knotTorus];
 
@@ -74,25 +74,43 @@ scene.add(directionalLight);
 /**
  * Scroll
  */
-let scrollY = window.scrollY
+let scrollY = window.scrollY;
+let currentSection = 0;
 
-window.addEventListener('scroll', () => 
-{
-    scrollY = window.scrollY
-})
+window.addEventListener("scroll", () => {
+  scrollY = window.scrollY;
+  const newSection = Math.round(scrollY / sizes.height);
+
+  if (newSection != currentSection) {
+    currentSection = newSection;
+
+    gsap.to(sectionMeshes[currentSection].rotation,
+    {
+        duration: 3,
+        ease: 'power2.inOut',
+        x: '+=6',
+        y: '+=3'
+    })
+  }
+});
 
 /**
  * Mouse
  */
-let cursorData = {}
-cursorData.x = 0
-cursorData.y = 0
+let cursorData = {};
+cursorData.x = 0;
+cursorData.y = 0;
 
-window.addEventListener('mousemove', (event) => 
-{
-    cursorData.x = event.clientX / sizes.width - 0.5 
-    cursorData.y = event.clientY / sizes.height - 0.5
-})
+window.addEventListener("mousemove", (event) => {
+  cursorData.x = event.clientX / sizes.width - 0.5;
+  cursorData.y = event.clientY / sizes.height - 0.5;
+});
+
+/**
+ * Particles
+ */
+const particleCount = 1000;
+const positions = new Float32Array(particleCount * 3);
 
 /**
  * SizYs
@@ -120,7 +138,7 @@ window.addEventListener("resize", () => {
  * Camera
  */
 // Base camera
-const cameraGroup = new THREE.Group()
+const cameraGroup = new THREE.Group();
 
 const camera = new THREE.PerspectiveCamera(
   35,
@@ -129,7 +147,7 @@ const camera = new THREE.PerspectiveCamera(
   100
 );
 camera.position.z = 6;
-cameraGroup.add(camera)
+cameraGroup.add(camera);
 scene.add(cameraGroup);
 
 /**
@@ -148,25 +166,28 @@ renderer.setClearAlpha(0); // Maneja la transparencia del canvas 0 - 1
  * Animate
  */
 const clock = new THREE.Clock();
+let previousTime = 0;
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+  const delta = elapsedTime - previousTime;
+  previousTime = elapsedTime;
 
   //Animate Meshes
   for (const mesh of sectionMeshes) {
-    mesh.rotation.x = elapsedTime / 4
-    mesh.rotation.y = elapsedTime / 4
+    mesh.rotation.x += delta * 0.1;
+    mesh.rotation.y += delta * 0.12;
   }
 
   //Animate Camera
-  camera.position.y = - scrollY / sizes.height * objectsDistance
+  camera.position.y = (-scrollY / sizes.height) * objectsDistance;
 
   //Parallax
-  const parallaxX = cursorData.x
-  const parallaxY = - cursorData.y
-  
-  cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 0.1
-  cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 0.1
+  const parallaxX = cursorData.x * 0.5;
+  const parallaxY = -cursorData.y * 0.5;
+
+  cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * delta;
+  cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * delta;
 
   // Render
   renderer.render(scene, camera);
