05b97cb543d610e3d051804a268f8bc8f309eea1
tomas96ech <techeverri96@gmail.com>
2024-07-06 20:58:36 -0500

    first R3F

---
+++ React Three Fiber/01-first-r3f-application/src/CustomObject.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const CustomObject = () => {
+  return (
+    <mesh>
+        <boxGeometry />
+        <meshStandardMaterial color={'red'} />
+    </mesh>
+  )
+}
--- React Three Fiber/01-first-r3f-application/src/Experience.jsx
+++ React Three Fiber/01-first-r3f-application/src/Experience.jsx
@@ -1,20 +1,47 @@
+import { extend, useFrame, useThree } from '@react-three/fiber'
 import React from 'react'
+import { useRef } from 'react'
+import { OrbitControls } from 'three/examples/jsm/Addons.js'
+import { CustomObject } from './CustomObject'
+
+extend({ OrbitControls })
 
 export const Experience = () => {
+
+    const cubeRef = useRef()
+    const group = useRef()
+
+    const { camera, gl } = useThree()
+
+    useFrame((state, delta) => {
+        cubeRef.current.rotation.y += delta / 4
+        //group.current.rotation.y += delta / 2
+    })
+
+
+
     return (
-        <group>
-            <mesh position-x={-2}>
-                <sphereGeometry args={[1, 25, 10]} />
-                <meshBasicMaterial color={'orange'} wireframe={true} />
-            </mesh>
-            <mesh position-x={2}>
-                <boxGeometry />
-                <meshBasicMaterial color={'purple'} wireframe={true} />
-            </mesh>
+        <>
+            <orbitControls args={[camera, gl.domElement]} />
+
+            <ambientLight intensity={2} />
+            <directionalLight position={[1, 2, 3]} intensity={1.5} />
+
+            <CustomObject />
+            <group ref={group}>
+                <mesh position-x={-2}>
+                    <sphereGeometry args={[1, 25, 10]} />
+                    <meshStandardMaterial color={'orange'} />
+                </mesh>
+                <mesh ref={cubeRef} position-x={2}>
+                    <boxGeometry />
+                    <meshStandardMaterial color={'purple'} />
+                </mesh>
+            </group>
             <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={8} >
                 <planeGeometry />
-                <meshBasicMaterial color={'green'} />
+                <meshStandardMaterial color={'green'} />
             </mesh>
-        </group>
+        </>
     )
 }
