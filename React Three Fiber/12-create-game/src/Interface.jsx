0a8d4165c239e56103980be8565872ecf7bb9dec
tomas96ech <techeverri96@gmail.com>
2024-07-21 16:40:11 -0500

    fix shadows

---
+++ React Three Fiber/12-create-game/src/Interface.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Interface = () => {
+  return (
+    <div className='interface'>
+        <div className="time">0.00</div>
+        <div className="restart">¿ Reiniciar ?</div>
+    </div>
+  )
+}
